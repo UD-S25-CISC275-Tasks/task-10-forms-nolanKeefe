bb01db3727ccde583dae79bb873f1996c81a2298
nolanKeefe <npkeefe@udel.edu>
2025-03-16 23:36:49 -0400

    I mean i pass all the tests

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,24 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [Answer, setAnswer] = useState<string>(options[0]);
+    function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+        setAnswer(event.target.value);
+    }
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="userEmotions">
+                <Form.Label>Question?</Form.Label>
+                <Form.Select value={options} onChange={updateAnswer}>
+                    {options.map((option: string) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            <div>{Answer === expectedAnswer ? "✔️" : "❌"}.</div>
         </div>
     );
 }
