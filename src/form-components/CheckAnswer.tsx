f7c6fa30f9ceda969d3101c4d893b728a7cc5bfb
nolanKeefe <npkeefe@udel.edu>
2025-03-16 20:18:23 -0400

    CheckAnswer complete

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
-
 export function CheckAnswer({
     expectedAnswer,
 }: {
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [Answer, setAnswer] = useState<string>("");
     return (
         <div>
             <h3>Check Answer</h3>
+            <input
+                type="textbox"
+                onChange={(event) => {
+                    setAnswer(event.target.value);
+                }}
+            />
+            <div>{Answer === expectedAnswer ? "✔️" : "❌"}.</div>
         </div>
     );
 }
