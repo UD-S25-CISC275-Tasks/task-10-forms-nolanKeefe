68bac794fa535ddaad06dedcee1425026095ab67
nolanKeefe <npkeefe@udel.edu>
2025-03-16 21:01:41 -0400

    Give attempts, edit mode started, Deleted Double half state again

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,20 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): React.JSX.Element {
+    const [EditMode, setEditMode] = useState<boolean>(false);
+    function updateEditMode(event: React.ChangeEvent<HTMLInputElement>) {
+        setEditMode(event.target.checked);
+    }
     return (
         <div>
             <h3>Edit Mode</h3>
+            <Form.Check
+                type="switch"
+                label="Edit Mode"
+                checked={EditMode}
+                onChange={updateEditMode}
+            />
         </div>
     );
 }
--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,45 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): React.JSX.Element {
+    const [Attempts, setAttempts] = useState<number>(3);
+    const [Request, setRequest] = useState<string>("");
+
+    function addAttempt(): void {
+        setAttempts(Number(Request) ? Attempts + Number(Request) : Attempts);
+    }
+    function subAttempt(): void {
+        setAttempts(Attempts - 1);
+    }
     return (
         <div>
             <h3>Give Attempts</h3>
+            <Button
+                onClick={() => {
+                    subAttempt();
+                }}
+                disabled={Attempts <= 0}
+            >
+                use
+            </Button>{" "}
+            attempts: {Attempts}
+            <Button
+                onClick={() => {
+                    addAttempt();
+                }}
+            >
+                gain
+            </Button>{" "}
+            <Form.Group controlId="formMovieReleased">
+                <Form.Label>Request:</Form.Label>
+                <Form.Control
+                    type="number"
+                    value={Request}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                        setRequest(event.target.value);
+                    }}
+                />
+            </Form.Group>
         </div>
     );
 }
