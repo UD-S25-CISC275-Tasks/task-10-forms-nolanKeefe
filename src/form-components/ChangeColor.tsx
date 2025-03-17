1d3024b57bd84e63b2b55c7db3c51fe5a9eee050
nolanKeefe <npkeefe@udel.edu>
2025-03-16 23:27:50 -0400

    Edit mode change color screw change color ggrrrrr

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,56 @@
 import React, { useState } from "react";
 
 export function ChangeColor(): React.JSX.Element {
+    const [color, setColor] = useState<string>("red");
+
+    const colors: string[] = [
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "purple",
+        "grey",
+        "magenta",
+    ];
+
     return (
         <div>
             <h3>Change Color</h3>
+            <div>
+                {colors.map((c) => (
+                    <label
+                        key={c}
+                        style={{ marginRight: "10px", cursor: "pointer" }}
+                    >
+                        <input
+                            type="radio"
+                            name="colors"
+                            value={c}
+                            checked={color === c}
+                            onChange={() => {
+                                setColor(c);
+                            }}
+                            style={{ marginRight: "5px" }}
+                        />
+                        {c}
+                    </label>
+                ))}
+            </div>
+            <p>You have chosen:</p>
+            <div
+                data-testid="colored-box"
+                style={{
+                    backgroundColor: color,
+                    padding: "10px",
+                    color: "white",
+                    display: "inline-block",
+                    borderRadius: "5px",
+                    fontWeight: "bold",
+                }}
+            >
+                {color}
+            </div>
         </div>
     );
 }
--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -3,18 +3,43 @@ import { Form } from "react-bootstrap";
 
 export function EditMode(): React.JSX.Element {
     const [EditMode, setEditMode] = useState<boolean>(false);
+    const [isStudent, setIsStudent] = useState<boolean>(true);
+    const [UserName, setUserName] = useState<string>("Your Name");
     function updateEditMode(event: React.ChangeEvent<HTMLInputElement>) {
         setEditMode(event.target.checked);
+    }
+    function updateIsStudent(event: React.ChangeEvent<HTMLInputElement>) {
+        setIsStudent(event.target.checked);
     }
     return (
         <div>
             <h3>Edit Mode</h3>
             <Form.Check
                 type="switch"
-                label="Edit Mode"
+                id="edit-mode"
                 checked={EditMode}
                 onChange={updateEditMode}
             />
+            {EditMode ?
+                <Form.Check
+                    type="checkbox"
+                    id="student-status"
+                    label="Student"
+                    checked={isStudent}
+                    onChange={updateIsStudent}
+                    disabled={!EditMode}
+                />
+            :   ""}
+            {EditMode ?
+                <input
+                    type="textbox"
+                    disabled={!EditMode}
+                    onChange={(event) => {
+                        setUserName(event.target.value);
+                    }}
+                />
+            :   ""}
+            {UserName} is {isStudent ? "a Student" : "not a Student"}
         </div>
     );
 }
